f31d77af531f3894e37ec9f0919f236c8792749b
Simon Crabtree <simon@simoncrabtree.net>
2012-09-21 23:53:28 +0100

    Added express and ejs. Now serves out a simple index page and loads a dojo main module

--- lima.js
+++ lima.js
@@ -1,1 +1,23 @@
-console.log("Welcome to Lima");
+var express = require('express'),
+    app;
+
+exports.createServer = function () {
+    app = express();
+    app.set('view engine', 'ejs');
+    app.set('views', process.env.PWD + '/server/views');
+    app.use(express.static(process.env.PWD + '/client'));
+
+
+    app.get('/', function (req, res) {
+        res.render('index');
+    });
+    console.log("Welcome to Lima");
+
+    return app;
+};
+
+exports.listen = function (customPort) {
+    var port = process.env.port || customPort || 3000;
+    app.listen(port);
+    console.log("Lima server running on port", port);
+};
